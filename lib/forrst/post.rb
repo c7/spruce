b2ce66a5b642b4ccc3af94777d966f28d0b45295
Peter Hellberg <peter@c7.se>
2010-08-26 04:18:19 +0200

    Make sure that the date atleast has a year (Write a better pattern tomorrow)

--- lib/forrst/post.rb
+++ lib/forrst/post.rb
@@ -18,23 +18,23 @@ module Forrst
         'markdown_description', 'markdown_content', 'reply_key', 'reply_url', 
         'current_user_likes_this', 'url', 'url_with_wbr', 'post_type', 
         'in_reply_to_post_id', 'post_url', 'phrase', 'formatted_description',
-        'tag_objs', 'tags'
+        'tag_objs', 'tags', 'images', 'snap_content_type',
       ]
       
       # Numeric fields such as like count and number of views
       numeric_fields = [
         'like_count', 'views', 'is_public', 'id', 'comment_count', 
-        'short_url_redirects', 'user_id'
+        'short_url_redirects', 'user_id', 'snap_file_size'
       ]
       
       # Date fields
       date_fields = [
-        'created_at', 'updated_at'
+        'created_at', 'updated_at', 'snap_updated_at'
       ]
       
       standard_fields.map { |f| instance_variable_set("@#{f}", data[f]) }
       numeric_fields.map  { |f| instance_variable_set("@#{f}", data[f].to_i) }
-      date_fields.map     { |f| instance_variable_set("@#{f}", Time.parse(data[f])) }
+      date_fields.map     { |f| instance_variable_set("@#{f}", f.match(/\d\d\d\d/) ? Time.parse(data[f]) : nil) }
       
       # Define accessor methods
       (class << self; self; end).class_eval do
